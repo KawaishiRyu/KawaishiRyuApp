3e149ec27976f33a7bb1c0e98ce0d0496040bc2a
EfrainMorales <eframorales421@gmail.com>
2023-01-19 07:58:35 -0300

    Implementacion de librerias y sdk de Android cambiado de 32 a 33 debido a errores e implemntacion de permisos de internet

--- app/src/main/java/app/kawaishiryu/jiujitsu/MainActivity.kt
+++ app/src/main/java/app/kawaishiryu/jiujitsu/MainActivity.kt
@@ -1,11 +1,18 @@
 package app.kawaishiryu.jiujitsu
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
